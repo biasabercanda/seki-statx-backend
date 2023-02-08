7569dc6c08dbe86efe4e951aedf5d69c41233403
AdmirallHabibi <sofyanhidayat48@gmail.com>
2023-02-08 11:08:46 +0700

    first commit

---
+++ api/helper_functions.py
@@ -0,0 +1,56 @@
+import requests
+from io import BytesIO
+import pandas as pd
+import calendar
+import datetime
+import xlrd
+
+def get_data(tabel):
+  url = "https://cors-proxy.sofyanhidayat48.workers.dev/?https://www.bi.go.id/SEKI/tabel/"+tabel+".xls"
+  response = requests.get(url).content
+  f = pd.ExcelFile(BytesIO(response))
+  return f
+
+def date(x):
+  if type(x) == datetime.datetime:
+    x = calendar.month_abbr[x.month]
+  return x
+
+def clean_data(df):
+  df1 = df
+  df1.dropna(axis=0,thresh=9,inplace=True)
+  df1 = df1.reset_index(drop=True)
+
+  if 'Unnamed: 1' in df1.columns:
+    df2 = df1[['Unnamed: 1','Unnamed: 2']] 
+    df1[['Unnamed: 1','Unnamed: 2']] = df2.fillna(axis=1,method='ffill')
+  
+  year = 0
+  cek = ['Jan', 'Q1']
+  for column in df1.columns:
+      if year == 0 and isinstance(df1[column][0], int):
+          year = df1[column][0]
+      elif df1[column][1] in cek:
+          year += 1
+      df1.loc[0, column] = year
+
+  df1.loc[1] = df1.loc[1].apply(date)
+
+  keterangan = df1.loc[0,df1.columns[2]]
+  df1.loc[0]= df1.loc[[0,1],df1.columns[1]:df1.columns[len(df1.columns)-1]].astype(str).apply('/'.join)
+  df1.loc[0,df1.columns[2]] = keterangan
+
+  d= df1.isna().any()
+  cols_to_ignore = [df1.columns[2]]
+  d[cols_to_ignore] = False
+  df1 = df1.loc[:,~d]
+
+  df1.dropna(inplace=True)
+
+  df1.columns = df1.loc[0]
+  df1.set_index(df1.columns[0],inplace=True)
+
+  df1.drop(df1.index[0],inplace=True)
+  df1.columns = df1.columns.str.replace('*','')
+
+  return df1
--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from api.helper_functions import get_data, date, clean_data
+from io import BytesIO
+import pandas as pd
+import xlrd
+
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+class seki(Resource):
+    def get(self,table_id):
+        f = get_data(table_id.upper())
+        sheets = f.sheet_names
+        df = f.parse(len(sheets)-1)
+        cleaned_data = clean_data(df)
+        res = cleaned_data.to_json(orient ='split')
+        return res
 
-@app.route('/about')
-def about():
-    return 'About'
+api.add_resource(seki, '/<string:table_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
