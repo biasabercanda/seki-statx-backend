d8185b90e613dbc4d1da2a7f7f66c26d50764dd0
Sofyan <Sofyan@sofys-MacBook-Air.local>
2023-05-16 15:53:36 +0700

    remove corelation function

--- api/helper_functions.py
+++ api/helper_functions.py
@@ -3,10 +3,7 @@ from io import BytesIO
 import pandas as pd
 import calendar
 import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from flask import send_file
+
 
 
 def get_data(tabel):
@@ -58,43 +55,3 @@ def clean_data(df):
   df1.columns = df1.columns.str.replace('*','')
 
   return df1
-
-def corelation(data1,data2,data_type=12,year_from=2010,year_to=2023):
-  base = 2010
-  f = (year_from-base)*12
-  t = f+data_type+(year_to-year_from)*data_type
-
-  y = data1[f:t]
-  x = data2 [f:t]
-
-  # Create and fit the linear regression model
-  model = LinearRegression()
-  model.fit(x.reshape(-1, 1), y)
-
-  # Get the slope and intercept of the regression line
-  slope = model.coef_[0]
-  intercept = model.intercept_
-
-  # Make predictions
-  x_new = np.array([6, 7, 8])  # New data points
-  y_pred = model.predict(x_new.reshape(-1, 1))
-
-  # Visualize the regression line and predictions
-  plt.scatter(x, y, label='Data')
-  plt.plot(x, model.predict(x.reshape(-1, 1)), color='red', label='Regression Line')
-  plt.xlabel('X')
-  plt.ylabel('Y')
-  plt.legend()
-
-  # Save the plot to a BytesIO object
-  image_stream = BytesIO()
-  plt.savefig(image_stream, format='png')
-  image_stream.seek(0)
-
-
-  return{
-     
-     'slope':slope,
-     'intercept':intercept,
-     'image': send_file(image_stream, mimetype='image/png')
-  }
